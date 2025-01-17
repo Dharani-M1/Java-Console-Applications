fb375cb05e37e2c0b27a92467005dc7a45b59bc4
Dharani-M1 <dharanimagesh1817@gmail.com>
2025-01-17 21:52:58 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-
-    public class Main {
-        public static void main(String[] args) {
-            ATMOperation atmOperation=new ATMOperation();
-            atmOperation.start();
-        }
-    }
